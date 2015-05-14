4b9487cccee1e9ad0bbe54d7de8fccfdd8368fe1
Maarten Huijsmans <maarten.huijsmans@gmail.com>
2015-05-14 12:22:27 +0200

    run it local

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
 
+# Run cmd.main() locally. Once installed through pip you'll have to use the 'poeditor' cmd.
 if __name__ == "__main__":
     from poeditor_client import cmd
     cmd.main()
